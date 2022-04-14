617c7df0c3378f1ce20cbc1cc470a8963a63a821
xhyrom <generalkubo@gmail.com>
2022-04-14 21:26:08 +0200

    update

--- scripts/update.mjs
+++ scripts/update.mjs
@@ -1,52 +1,72 @@
-import simpleGit from 'simple-git';
 import path from 'node:path';
 import fs from 'node:fs/promises';
 import { fetch } from 'undici';
-import { constants } from 'node:fs';
+import { existSync } from './utils/existSync.mjs';
+import { pushFiles } from './utils/pushFiles.mjs';
+import { markdownTable } from 'markdown-table';
 
 const discordToken = process.env.DISCORD_TOKEN;
-const git = simpleGit({ baseDir: path.resolve('..') });
 
-const pushFiles = async() => {
-    const result = await git.status();
-    if (result.files.length === 0) {
-      console.log('No changes');
-      return;
-    }
-
-    await git.add(files);
-    await git.commit('Update activities 🚀');
-    await git.push('origin', 'master');
-}
-
-const existSync = async() => {
-    try {
-        await fs.access(`${path.resolve('..')}/activities/`, constants.R_OK | constants.W_OK);
-        return true;
-    } catch {
-        return false;
-    }
-}
-
-const activities = await fetch('https://discord.com/api/v9/activities/guilds/831646372519346186/config', {
+const appIds = (await (await fetch('https://discord.com/api/v9/activities/guilds/831646372519346186/config', {
     headers: {
         'Authorization': discordToken
     }
-})
+})).json()).app_ids
 
-if (!(await existSync())) await fs.mkdir(`${path.resolve('..')}/activities/`)
+if (!(await existSync())) await fs.mkdir(`${path.resolve('..')}/activities/`);
 
+const activities = [];
 const files = [];
-const app_ids = (await activities.json()).app_ids;
-for (const appId of app_ids) {
-    const assetsPromise = (await fetch(`https://canary.discord.com/api/v9/oauth2/applications/${appId}/assets`)).json();
-    const assets = await assetsPromise;
+
+for (const appId of appIds) {
+    const applicationInfo = await (await fetch(`https://canary.discord.com/api/v9/oauth2/authorize?client_id=${appId}`, {
+        headers: {
+            'Authorization': discordToken
+        }
+    })).json();
+
+    const assets = await (await fetch(`https://canary.discord.com/api/v9/oauth2/applications/${appId}/assets`)).json();
 
     const image = await fetch(`https://cdn.discordapp.com/app-assets/${appId}/${assets.find((asset) => asset.name === 'embedded_cover').id}.png?size=1024`);
-    const name = `${path.resolve('..')}/activities/${appId}.png`;
+    const nameImage = `${path.resolve('..')}/activities/${appId}.png`;
+    const nameInfo = `${path.resolve('..')}/activities/${appId}.json`
 
-    files.push(name);
-    fs.writeFile(name, Buffer.from(await image.arrayBuffer()).toString('base64'), 'base64');
+    activities.push({
+        id: appId,
+        name: applicationInfo.application?.name,
+        icon: applicationInfo.application?.icon,
+        description: applicationInfo.application?.description,
+        activity_config: applicationInfo.application.embedded_activity_config
+    });
+
+    files.push(nameInfo);
+    files.push(nameImage);
+
+    fs.writeFile(
+        nameInfo,
+        JSON.stringify(
+            {
+                id: appId,
+                name: applicationInfo.application?.name,
+                description: applicationInfo.application?.description,
+                icon: applicationInfo.application?.icon,
+                activity_config: applicationInfo.application.embedded_activity_config
+            },
+            null,
+            4
+        )
+    );
+
+    fs.writeFile(nameImage, Buffer.from(await image.arrayBuffer()).toString('base64'), 'base64');
+    console.log(`Activity ${applicationInfo.application?.name} (${appId}) updated. 🚀`);
 }
 
-pushFiles();
+fs.writeFile(`${path.resolve('..')}/activities.json`, JSON.stringify(activities, null, 4));
+fs.writeFile(`${path.resolve('..')}/activities.md`, markdownTable(
+    [
+        ['Application Id', 'Application Name', 'Premium Tier', 'Application Description', 'Application Icon', 'Image'],
+        ...activities.map((activity) => [activity.id, activity.name, activity.activity_config.activity_premium_tier_level, activity.description, activity.icon, `[${activity.name}](./activities/${activity.id}.png)`])
+    ]
+))
+
+pushFiles(files);
---
+++ scripts/utils/pushFiles.mjs
@@ -0,0 +1,15 @@
+import simpleGit from 'simple-git';
+import path from 'node:path';
+const git = simpleGit({ baseDir: path.resolve('..') });
+
+export const pushFiles = async(files) => {
+    const result = await git.status();
+    if (result.files.length === 0) {
+      console.log('No changes');
+      return;
+    }
+
+    //await git.add(files);
+    //await git.commit('Update activities 🚀');
+    //await git.push('origin', 'master');
+}
