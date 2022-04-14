1d9c5fe4fdc3f7dadf4dffdc002579300cc17e63
xhyrom <generalkubo@gmail.com>
2022-04-14 20:23:07 +0200

    replace octokit

--- scripts/update.mjs
+++ scripts/update.mjs
@@ -1,5 +1,6 @@
-import { Octokit } from "octokit";
-import { fetch } from "undici";
+import simpleGit from 'simple-git';
+import fs from 'fs/promises';
+import { fetch } from 'undici';
 
 const githubToken = process.env.GITHUB_TOKEN;
 const discordToken = process.env.DISCORD_TOKEN;
@@ -13,30 +14,30 @@ const activities = await fetch('https://discord.com/api/v9/activities/guilds/831
         'Authorization': discordToken
     }
 })
-(await import('octokit-commit-multiple-files'))(activities);
 
-const files = {};
+const files = [];
 const app_ids = (await activities.json()).app_ids;
 for (const appId of app_ids) {
     const assetsPromise = (await fetch(`https://canary.discord.com/api/v9/oauth2/applications/${appId}/assets`)).json();
     const assets = await assetsPromise;
 
     const image = await fetch(`https://cdn.discordapp.com/app-assets/${appId}/${assets.find((asset) => asset.name === 'embedded_cover').id}.png?size=1024`);
-    const base64 = Buffer.from(await image.arrayBuffer()).toString('base64');
+    const name = `./activities/${appId}.png`
 
-    files[`activities/${appId}.png`] = {
-        contents: base64
-    }
+    files.push(name);
+    fs.writeFile(name, Buffer.from(await image.arrayBuffer()));
 }
 
-client.repos.createOrUpdateFiles({
-    owner: 'xHyroM',
-    repo: 'discord-activities',
-    branch: 'master',
-    changes: [
-        {
-            message: 'Update activities 🚀',
-            files
-        }
-    ]
-})
+const pushFiles = async() => {
+    const result = await git.status();
+    if (result.files.length === 0) {
+      console.log('No changes');
+      return;
+    }
+
+    await git.add(files);
+    await git.commit('Update activities 🚀');
+    await git.push('origin', 'master');
+}
+
+pushFiles();
