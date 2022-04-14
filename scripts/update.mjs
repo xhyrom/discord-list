05bc77d7e950a081e28c5251ad9dee2ce4919f6d
xhyrom <generalkubo@gmail.com>
2022-04-14 20:30:52 +0200

    fix

--- scripts/update.mjs
+++ scripts/update.mjs
@@ -4,30 +4,8 @@ import fs from 'node:fs/promises';
 import { fetch } from 'undici';
 import { constants } from 'node:fs';
 
-const git = simpleGit({ baseDir: path.resolve('..') });
-
 const discordToken = process.env.DISCORD_TOKEN;
-
-const activities = await fetch('https://discord.com/api/v9/activities/guilds/831646372519346186/config', {
-    headers: {
-        'Authorization': discordToken
-    }
-})
-
-if (!(await existSync())) await fs.mkdir('./activities/')
-
-const files = [];
-const app_ids = (await activities.json()).app_ids;
-for (const appId of app_ids) {
-    const assetsPromise = (await fetch(`https://canary.discord.com/api/v9/oauth2/applications/${appId}/assets`)).json();
-    const assets = await assetsPromise;
-
-    const image = await fetch(`https://cdn.discordapp.com/app-assets/${appId}/${assets.find((asset) => asset.name === 'embedded_cover').id}.png?size=1024`);
-    const name = `./activities/${appId}.png`
-
-    files.push(name);
-    fs.writeFile(name, Buffer.from(await image.arrayBuffer()));
-}
+const git = simpleGit({ baseDir: path.resolve('..') });
 
 const pushFiles = async() => {
     const result = await git.status();
@@ -48,6 +26,27 @@ const existSync = async() => {
     } catch {
         return false;
     }
+}
+
+const activities = await fetch('https://discord.com/api/v9/activities/guilds/831646372519346186/config', {
+    headers: {
+        'Authorization': discordToken
+    }
+})
+
+if (!(await existSync())) await fs.mkdir('./activities/')
+
+const files = [];
+const app_ids = (await activities.json()).app_ids;
+for (const appId of app_ids) {
+    const assetsPromise = (await fetch(`https://canary.discord.com/api/v9/oauth2/applications/${appId}/assets`)).json();
+    const assets = await assetsPromise;
+
+    const image = await fetch(`https://cdn.discordapp.com/app-assets/${appId}/${assets.find((asset) => asset.name === 'embedded_cover').id}.png?size=1024`);
+    const name = `./activities/${appId}.png`
+
+    files.push(name);
+    fs.writeFile(name, Buffer.from(await image.arrayBuffer()));
 }
 
 pushFiles();
